d9f356c98cb16eefbdef7871f4cb7ed35380eb99
DabbiSabbi <diegpeniarol@hotmail.com>
2026-09-23 20:57:45 -0400

    Inventory fix.

---
+++ Scripts/grid_container.gd
@@ -0,0 +1,18 @@
+extends GridContainer
+var current_slot = 0
+
+func _ready() -> void:
+	print("Loaded")
+
+func slot_selection(a,b):
+	if Input.is_action_just_pressed(a):
+		get_child(current_slot).slot_high.visible = false
+		get_child(b).slot_select()
+		current_slot = b
+
+func _process(delta):
+	slot_selection("slot_1", 0)
+	slot_selection("slot_2", 1)
+	slot_selection("slot_3", 2)
+	slot_selection("slot_4", 3)
+	slot_selection("slot_5", 4)
